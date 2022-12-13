155526970feb4770940cc269eb372a0b2557ef25
Aaron <aaronmathew286@gmail.com>
2022-12-13 14:24:02 +0530

    comment was created but not yet migrated

--- petinaries/products/models.py
+++ petinaries/products/models.py
@@ -1,31 +1,9 @@
 from django.db import models
+from home.models import PetProducts
 
-class petpro:
-    name :str
-    price:int
-    img :str
+class comment(models.Model):
 
-pro1=petpro()
-pro1.name="Cat Food"
-pro1.price=1200
-pro1.img="product-2.png"
-
-pro2=petpro()
-pro2.name="Cat Food"
-pro2.price=1200
-pro2.img="product-3.png"
-
-pro3=petpro()
-pro3.name="Cat Food"
-pro3.price=1200
-pro3.img="product-4.png"
-
-pro4=petpro()
-pro4.name="Cat Food"
-pro4.price=1200
-pro4.img="product-5.png"
-
-pro5=petpro()
-pro5.name="Cat Food"
-pro5.price=1200
-pro5.img="product-6.png"
+    pro=models.ForeignKey(PetProducts,related_name="comments",on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    cmt=models.TextField()
+    date=models.DateTimeField(auto_now_add=True)
