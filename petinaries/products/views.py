7b7293bd8127847faa69251b3a894b0fe61ff35d
neeraja <neerajasv2@gmail.com>
2022-12-21 04:13:19 +0530

    length controlling

--- petinaries/products/views.py
+++ petinaries/products/views.py
@@ -11,6 +11,24 @@ def detail(request):
 
     data=PetProducts.objects.get(id=id)
     total=int(data.price)-(int(data.price)*int(data.discount)/100)
-    return render(request,"detail.html",{"pro":data,"total":total})
+
+    if "his" in request.session:
+        if id in request.session["his"]:
+            request.session["his"].remove(id)
+            request.session["his"].insert(0,id)
+        else:
+            request.session["his"].insert(0,id)
+        if len(request.session["his"])>4:
+            request.session["his"].pop()
+
+        print(request.session["his"])
+        prop=PetProducts.objects.filter(id__in=request.session["his"])
+        print(prop)
+        request.session.modified=True
+        return render(request,"detail.html",{"pro":data,"total":total,"abc":prop})
+    else:
+        print("hello")
+        request.session["his"]=[id]
+        return render(request,"detail.html",{"pro":data,"total":total})
 
 
